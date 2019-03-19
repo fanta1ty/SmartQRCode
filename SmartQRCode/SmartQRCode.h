697edabc2e26f2752fa82a5331730f776fa72108
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2019-03-19 16:11:15 +0700

    Initial Commit

---
+++ SmartQRCode/SmartQRCode.h
@@ -0,0 +1,19 @@
+//
+//  SmartQRCode.h
+//  SmartQRCode
+//
+//  Created by Thinh Nguyen on 3/19/19.
+//  Copyright © 2019 Thinh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmartQRCode.
+FOUNDATION_EXPORT double SmartQRCodeVersionNumber;
+
+//! Project version string for SmartQRCode.
+FOUNDATION_EXPORT const unsigned char SmartQRCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartQRCode/PublicHeader.h>
+
+
